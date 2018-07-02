f515668246e59cfb5ebdbe1b837c5aac69a2813d
RobertoHer <00162317@uca.edu.sv>
2018-07-01 22:42:14 -0500

    Agregue los atributos

--- src/Entidades/CategoriaMarca.java
+++ src/Entidades/CategoriaMarca.java
@@ -13,4 +13,30 @@ public class CategoriaMarca {
     public CategoriaMarca(){
         
     }
+
+    public int getIdCategoriaMarca() {
+        return idCategoriaMarca;
+    }
+
+    public void setIdCategoriaMarca(int idCategoriaMarca) {
+        this.idCategoriaMarca = idCategoriaMarca;
+    }
+
+    public String getNombreCategoriaMarca() {
+        return nombreCategoriaMarca;
+    }
+
+    public void setNombreCategoriaMarca(String nombreCategoriaMarca) {
+        this.nombreCategoriaMarca = nombreCategoriaMarca;
+    }
+
+    public String getDescripcionCategoriaMarca() {
+        return descripcionCategoriaMarca;
+    }
+
+    public void setDescripcionCategoriaMarca(String descripcionCategoriaMarca) {
+        this.descripcionCategoriaMarca = descripcionCategoriaMarca;
+    }
+    private int idCategoriaMarca;
+    private String nombreCategoriaMarca, descripcionCategoriaMarca;
 }
--- src/Entidades/CategoriaProducto.java
+++ src/Entidades/CategoriaProducto.java
@@ -10,5 +10,30 @@ package Entidades;
  * @author LN710Q
  */
 public class CategoriaProducto {
-    
+
+    public int getIdCategoriaProducto() {
+        return idCategoriaProducto;
+    }
+
+    public void setIdCategoriaProducto(int idCategoriaProducto) {
+        this.idCategoriaProducto = idCategoriaProducto;
+    }
+
+    public String getNombreCategoriaProducto() {
+        return nombreCategoriaProducto;
+    }
+
+    public void setNombreCategoriaProducto(String nombreCategoriaProducto) {
+        this.nombreCategoriaProducto = nombreCategoriaProducto;
+    }
+
+    public String getDescripcionCategoriaProducto() {
+        return descripcionCategoriaProducto;
+    }
+
+    public void setDescripcionCategoriaProducto(String descripcionCategoriaProducto) {
+        this.descripcionCategoriaProducto = descripcionCategoriaProducto;
+    }
+    private int idCategoriaProducto;
+    private String nombreCategoriaProducto, descripcionCategoriaProducto;
 }
