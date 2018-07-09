85198a899a14a45b1c5593eb159c947b7ca9dedd
RobertoHer <00162317@uca.edu.sv>
2018-07-09 00:03:47 -0500

    Se agrego la clase Validar solo numero y solo letras

---
+++ src/Clases/Validar.java
@@ -0,0 +1,38 @@
+package Clases;
+
+
+
+
+import java.awt.event.KeyEvent;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author UCA
+ */
+public class Validar {
+    Character s;
+    
+    public void soloLetras(KeyEvent evt){
+        s = evt.getKeyChar();
+        if(!Character.isLetter(s)){
+            evt.consume();
+        }
+        
+        
+    }
+    public void soloNumeros(KeyEvent evt){
+        s = evt.getKeyChar();
+        if(!Character.isDigit(s)){
+            evt.consume();
+        }
+        
+        
+    }
+    
+}
